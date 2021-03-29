858b54dcb8ff759d1ab92d0c44bb932785e36f05
Haloger7 <54002440+Haloger7@users.noreply.github.com>
2021-03-29 12:41:11 +0300

    Little Update 🌀

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 document.getElementById("button").onclick = function () {
     let letters = '0123456789ABCDEF';
     const color = "#" + Math.random(letters).toString(16).slice(2,8);
-    document.body.style.background = color;
     document.getElementById("colorhex").innerHTML = color;
+    document.body.style.background = color;
 }
